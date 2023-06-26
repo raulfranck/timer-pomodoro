797912ff045a4b82bbc148c7e3cc9d669ed86cf2
raul franck <raulengfranck@gmail.com>
2023-06-26 15:37:17 -0300

    incluindo datalist no input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,25 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para a sua tarefa" />
-
+          <TaskInput
+            id="task"
+            list="task-suggenstions"
+            placeholder="Dê um nome para a sua tarefa"
+          />
+          <datalist id="task-suggenstions">
+            <option value="projeto 1"></option>
+            <option value="projeto 2"></option>
+            <option value="projeto 3"></option>
+            <option value="projeto 4"></option>
+          </datalist>
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmaountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
